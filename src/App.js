343a48bccbafe678645207b00293790529110c1a
Marco Terrana <marcoterrana45gmail.com>
2023-04-20 19:49:58 +0200

    another try

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React & more
+          Turazza!!!!!!!
         </a>
       </header>
     </div>
