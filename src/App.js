60493659d5ffab51d93a65ff328844a3c8646ced
Marco Terrana <marcoterrana45gmail.com>
2023-04-20 19:37:36 +0200

    web service v

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in a bit...
+          React & more
         </a>
       </header>
     </div>
